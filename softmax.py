50b60e814a7595cd9623a1d5f3951305a6ad6bbe
mindlab <cosmicascendant@gmail.com>
2018-12-28 00:04:56 -0800

    first commit

---
+++ softmax.py
@@ -0,0 +1,20 @@
+import math
+
+#softmax test
+
+def softmax(*input_vector):
+	ea_vector=[]
+	for a in input_vector:
+		ea = (math.e ** a) 
+		ea_vector.append(ea)
+		vector_sum = sum(ea_vector)
+	for k in ea_vector:
+		result = k / vector_sum
+		print('P:',round(result,2))
+	print('input_vector:',input_vector)
+	return 'Softmax results'
+
+
+print(softmax(.04,.29,1,.44))
+
+
